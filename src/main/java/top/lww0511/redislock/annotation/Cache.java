94e46f1c39ad96daa2137f8a96838625855cb8fa
lww <lerder.liu@shebaochina.com>
2021-01-20 10:04:33 +0800

    添加时间单位

--- src/main/java/top/lww0511/redislock/annotation/Cache.java
+++ src/main/java/top/lww0511/redislock/annotation/Cache.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 添加缓存
@@ -22,5 +23,10 @@ public @interface Cache {
      * @return 锁定时间，默认1分钟
      */
     int value() default 1;
+
+    /**
+     * @return 时间单位
+     */
+    TimeUnit unit() default TimeUnit.MINUTES;
 
 }
